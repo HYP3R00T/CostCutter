06ecf08700574c4842d029af0d3d27a72eaac6dc
Hyperoot <rajesh@hyperoot.dev>
2025-10-07 05:11:50 +0000

    feat(ec2): add security group cleanup functionality

--- src/costcutter/services/ec2/__init__.py
+++ src/costcutter/services/ec2/__init__.py
@@ -2,8 +2,9 @@ from boto3.session import Session
 
 from costcutter.services.ec2.instances import cleanup_instances
 from costcutter.services.ec2.key_pairs import cleanup_key_pairs
+from costcutter.services.ec2.security_groups import cleanup_security_groups
 
-_HANDLERS = {"instances": cleanup_instances, "key_pairs": cleanup_key_pairs}
+_HANDLERS = {"instances": cleanup_instances, "key_pairs": cleanup_key_pairs, "security_group": cleanup_security_groups}
 
 
 def cleanup_ec2(session: Session, region: str, dry_run: bool = True, max_workers: int = 1):
